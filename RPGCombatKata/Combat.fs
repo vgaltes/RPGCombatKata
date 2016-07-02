a6d862e0e1da04ae087639011ad29650f30b79c5
Vicenç García Altés <vincentredrum@gmail.com>
2016-07-02 14:17:55 +0100

    Refactor

--- RPGCombatKata/Combat.fs
+++ RPGCombatKata/Combat.fs
@@ -23,17 +23,20 @@ type Player =
 [<Literal>]
 let HealthLimit = 1000
 
-let sameCharacterNotAllowed characterFrom characterTo distance amount =
-    if characterFrom = characterTo then None
-    else Some(amount)
+let sameCharacterNotAllowed characterFrom playerTo distance amount =
+    match playerTo with
+    | Thing _ -> Some(amount)
+    | Character c ->
+        if characterFrom = c then None
+        else Some(amount)
 
 
-let sameCharacterAllowed characterFrom characterTo distance amount =
-    if characterFrom = characterTo then Some(amount)
+let sameCharacterAllowed characterFrom playerTo distance amount =
+    if characterFrom = playerTo then Some(amount)
     else None
 
 
-let distance characterFrom distance amount =
+let distance characterFrom (playerTo: Player) distance amount =
     let effectiveRange = 
         match characterFrom.Type with
         | Melee -> 2
@@ -43,123 +46,103 @@ let distance characterFrom distance amount =
     else Some(amount)
 
 
-let level characterFrom characterTo distance amount =
-    match characterFrom.Level - characterTo.Level with
-    | d when d <= -5 -> Some( amount / 2 )
-    | d when d >= 5 -> Some(amount + amount / 2)
-    | _ -> Some(amount)
+let level characterFrom playerTo distance amount =
+    match playerTo with
+    | Thing _ -> Some(amount)
+    | Character c ->
+        match characterFrom.Level - c.Level with
+        | d when d <= -5 -> Some( amount / 2 )
+        | d when d >= 5 -> Some(amount + amount / 2)
+        | _ -> Some(amount)
 
 
-let noLessThanZero health amount =
+let noLessThanZero characterFrom playerTo distance amount =
+    let health =
+        match playerTo with
+        | Character c -> c.Health
+        | Thing t -> t.Health
+
     if amount > health then Some (health)
     else Some (amount)
 
 
-let noMoreThanOneThousand health amount =
-    if amount < 1000 - health then Some (amount)
-    else Some (1000 - health)
+let noMoreThanOneThousand characterFrom playerTo distance amount =
+    match playerTo with
+    | Thing t -> None
+    | Character c ->
+        if amount < 1000 - c.Health then Some (amount)
+        else Some (1000 - c.Health)
 
-let sameFactionNotAllowed characterFrom characterTo distance amount =
-    let setFrom = set characterFrom.Factions
-    let setTo = set characterTo.Factions
-
-    if setFrom |> Set.isEmpty && setTo |> Set.isEmpty then Some(amount)
-    elif Set.intersect setFrom setTo |> Set.isEmpty then Some(amount)
-    else None
-
-let sameFactionOrSameCharacterAllowed characterFrom characterTo distance amount =
-    if characterFrom = characterTo then Some(amount) //There's another way to do this?
-    else
+let sameFactionNotAllowed characterFrom playerTo distance amount =
+    match playerTo with
+    | Thing _ -> Some(amount)
+    | Character c ->
         let setFrom = set characterFrom.Factions
-        let setTo = set characterTo.Factions
+        let setTo = set c.Factions
 
-        if not ( Set.intersect setFrom setTo |> Set.isEmpty ) then Some(amount)
+        if setFrom |> Set.isEmpty && setTo |> Set.isEmpty then Some(amount)
+        elif Set.intersect setFrom setTo |> Set.isEmpty then Some(amount)
         else None
 
-let noHealIfDead health amount =
-    if health = 0 then None
-    else Some(amount)
+let sameFactionOrSameCharacterAllowed characterFrom playerTo distance amount =
+    match playerTo with
+    | Thing _ -> None
+    | Character c ->
+        if characterFrom = c then Some(amount) //There's another way to do this?
+        else
+            let setFrom = set characterFrom.Factions
+            let setTo = set c.Factions
+
+            if not ( Set.intersect setFrom setTo |> Set.isEmpty ) then Some(amount)
+            else None
+
+let noHealIfDead characterFrom playerTo distance amount =
+    match playerTo with
+    | Thing _ -> None
+    | Character c ->
+        if c.Health = 0 then None
+        else Some(amount)
 
 let attackRules = [
     sameCharacterNotAllowed
     sameFactionNotAllowed
     level
-]
-
-let environmentalAttackRules = [
     distance
-]
-
-
-let attackLimitRules =[
     noLessThanZero
 ]
 
 let healRules = [
     sameFactionOrSameCharacterAllowed
-]
-
-let healLimitRules = [
     noMoreThanOneThousand
     noHealIfDead
 ]
 
-let rec calculateHealthDifference value rules characterFrom characterTo distance =
+let applyDamage damage player =
+    match player with
+    | Thing t -> Player.Thing {t with Health = t.Health - damage}
+    | Character c -> Player.Character {c with Health = c.Health - damage}
+
+
+let applyHeal healing player =
+    match player with
+    | Thing t -> Player.Thing {t with Health = t.Health + healing}
+    | Character c -> Player.Character {c with Health = c.Health + healing}
+
+
+let rec applyRules value rules characterFrom playerTo distance =
     match rules with
     | [] -> value
     | x::xs ->
-        let result = x characterFrom characterTo distance value
+        let result = x characterFrom playerTo distance value
         match result with
         | None -> 0
-        | Some v -> calculateHealthDifference v xs characterFrom characterTo distance
-
-
-let rec applyLimitRules value rules health =
-    match rules with
-    | [] -> value
-    | x::xs ->
-        let result = x health value
-        match result with
-        | None -> 0
-        | Some v -> applyLimitRules v xs health
-
-let rec applyEnvironmentalRules value rules characterFrom distance =
-    match rules with
-    | [] -> value
-    | x::xs ->
-        let result = x characterFrom distance value
-        match result with
-        | None -> 0
-        | Some v -> applyEnvironmentalRules v xs characterFrom distance
+        | Some v -> applyRules v xs characterFrom playerTo distance
 
 let interactWith characterFrom playerTo action distance =
-    let calculateDamage characterFrom playerTo action distance =
-        match action with
-        | Attack amount -> 
-            match playerTo with
-            | Character c ->
-                let damage = calculateHealthDifference amount attackRules characterFrom c distance
-                let damage' = applyEnvironmentalRules damage environmentalAttackRules characterFrom distance
-                applyLimitRules damage' attackLimitRules c.Health
-            | Thing t ->
-                let damage = applyEnvironmentalRules amount environmentalAttackRules characterFrom distance
-                applyLimitRules damage attackLimitRules t.Health
-
-        | Heal amount ->
-            match playerTo with
-            | Character c -> 
-                let healing = calculateHealthDifference amount healRules characterFrom c distance
-                applyLimitRules healing healLimitRules c.Health
-            | Thing t -> 0
-
-    let damage = calculateDamage characterFrom playerTo action distance
-
-    match action with 
-    | Attack _ ->
-        match playerTo with
-        | Character c -> Player.Character {c with Health = c.Health - damage}
-        | Thing t -> Player.Thing {t with Health = t.Health - damage}
-    | Heal _ ->
-        match playerTo with
-        | Character c -> Player.Character {c with Health = c.Health + damage}
-        | Thing t -> Player.Thing t
+    match action with
+    | Attack amount ->
+        let damage = applyRules amount attackRules characterFrom playerTo distance
+        playerTo |> applyDamage damage 
+    | Heal amount ->
+        let healing = applyRules amount healRules characterFrom playerTo distance
+        playerTo |> applyHeal healing
